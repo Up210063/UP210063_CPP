985db90b7c6392d10f2cbb6e564393f59a762718
up210063 <up210063@alumnos.upa.edu.mx>
2022-09-12 11:58:26 -0500

    programa

---
+++ U1/01_Holamundo.cpp
@@ -0,0 +1,30 @@
+/*  CABECERA DE PROGRAMA
+Date: 12/09/2022
+Author:Jesùs Alejandro Chàvez Macìas
+Description: Mi primer programa en C++
+Email: up210063@alumnos.upa.edu.mx
+Last modification: ##
+*/
+
+ /*
+ iostream = input /output 
+ stream(flujo)
+ */
+
+//------------PREPROCESSOR DIRECTIVES--------------------
+//input/output library between the screen & keyboard
+
+#include <iostream> 
+
+//---------------MAIN FUNCTION--------------- 
+ int main (){
+
+    //print on screen the text
+    std::cout << "Hello World!! This is my first program in cpp(c++)";
+
+
+
+    //Return 0 means that the programm ends succesfully
+    return 0;
+ }
+
