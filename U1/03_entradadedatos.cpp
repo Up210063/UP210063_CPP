34b73e0df86b6fed8b243ec6c72ff2404007aeb5
up210063 <up210063@alumnos.upa.edu.mx>
2022-09-21 17:48:44 -0500

    entrada de datos

--- U1/03_entradadedatos.cpp
+++ U1/03_entradadedatos.cpp
@@ -12,7 +12,10 @@ Last modification: ##
 using namespace std;
 
 int main (){
-int entero; float flotante;
+int entero; 
+float flotante; 
+char caracter;
+double grande=3/2;
 
 cout << "Dame un numero entero : " <<endl;
 cin >> entero;
@@ -27,6 +30,18 @@ cout << "El numero decimal ingresado es: " << flotante <<endl;
 cout << "El tamaño del decimal es: " << sizeof(flotante) << "bytes" << endl;
 
 cout << "============================================" << endl;
+
+cout << "Dame un caracter: " << endl;
+cin >> caracter;
+cout << "tu caracter es: " << caracter << endl;
+cout << "El tamaño de tu caracter es: " << sizeof(caracter) << "bytes" << endl;
+
+cout << "============================================" << endl;
+
+cout << "Dame un numero " << endl;
+cin >> grande;
+cout << "tu caracter es: " << grande<< endl;
+cout << "El tamaño de tu caracter es: " << sizeof(grande) << "bytes" << endl;
 
 return 0;
 }
