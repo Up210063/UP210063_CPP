23a25cc43cfe711eecd94ee609512bbbab2c9c22
up210063 <up210063@alumnos.upa.edu.mx>
2022-09-21 10:42:54 -0500

    entrada de datos

--- U1/03_entradadedatos.cpp
+++ U1/03_entradadedatos.cpp
@@ -24,6 +24,7 @@ cout << "============================================" << endl;
 cout << "Ingresa un nùmero decimal: " << endl;
 cin >> flotante;
 cout << "El numero decimal ingresado es: " << flotante <<endl;
+cout << "El tamaño del decimal es: " << sizeof(flotante) << "bytes" << endl;
 
 cout << "============================================" << endl;
 
