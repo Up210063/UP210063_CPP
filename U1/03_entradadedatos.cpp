c9aa24aa074a22e435fdd94bf18f62a9ede2227b
up210063 <up210063@alumnos.upa.edu.mx>
2022-09-21 09:55:44 -0500

    entrada de datos

---
+++ U1/03_entradadedatos.cpp
@@ -0,0 +1,23 @@
+/*  CABECERA DE PROGRAMA
+Date:20/09/2022
+Author:UP210063 J* A* C* M*
+Description: entrada de datos 
+Email: up210063@alumnos.upa.edu.mx
+Last modification: ##
+*/
+
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main (){
+int numero; 
+
+cout << "Dame un numero: " <<endl;
+cin >> numero;
+
+cout << "El numero ingresado es: " << numero << endl;
+
+return 0;
+}
